067b0fab9efe35424cef37c32cddbc16b439cbf9
German <gedaniel1@hotmail.com>
2023-04-04 21:37:30 -0300

    mejora en codigo js con cambio en funciones usando metodos

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var frase = "";
-var textarea = document.querySelector(".msjInicial");
+let frase = "";
+let matrizDeCambio = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+let textarea = document.querySelector(".msjInicial");
 textarea.focus();
-var encriptado = document.querySelector(".msjFinal");
+let encriptado = document.querySelector(".msjFinal");
 
 document.getElementById("aparece-texto").style.display = 'none';
 
@@ -10,36 +11,16 @@ function encriptar() {
 
     frase = "";
     if (validar()) {
-        for (var i = 0; i < textarea.value.length; i++) {
-            if (textarea.value[i] == "e") {
-                frase = frase + "enter";
-                continue;
-            }
-            if (textarea.value[i] == "i") {
-                frase = frase + "imes";
-                continue;
-            }
-            if (textarea.value[i] == "a") {
-                frase = frase + "ai";
-                continue;
-            }
-            if (textarea.value[i] == "o") {
-                frase = frase + "ober";
-                continue;
-            }
-            if (textarea.value[i] == "u") {
-                frase = frase + "ufat";
-                continue;
-            }
-            else {
-                frase = frase + textarea.value[i];
+        for (let i = 0; i < matrizDeCambio.length; i++) {
+            if(textarea.value.includes(matrizDeCambio[i][0])) {
+                textarea.value = textarea.value.replaceAll(matrizDeCambio[i][0], matrizDeCambio[i][1])
             }
         }
     } else {
-        alert("Solo ingresar letras y en minúscula, sin acentos");
+        alert("Solo ingresar letras en minúscula y sin acentos o caracteres especiales");
         textarea.focus();
     }
-
+    frase = textarea.value;
     encriptado.innerHTML = frase;
     textarea.value = "";
     aparece();
@@ -48,45 +29,23 @@ function encriptar() {
 function desencriptar() {
 
     frase = "";
-    for (var i = 0; i < textarea.value.length; i++) {
-        if ((textarea.value[i] == "e") && (textarea.value[i + 1] == "n") && (textarea.value[i + 2] == "t") && (textarea.value[i + 3] == "e") && (textarea.value[i + 4] == "r")) {
-            frase = frase + "e";
-            i = i + 4;
-            continue;
-        }
-        if ((textarea.value[i] == "i") && (textarea.value[i + 1] == "m") && (textarea.value[i + 2] == "e") && (textarea.value[i + 3] == "s")) {
-            frase = frase + "i";
-            i = i + 3;
-            continue;
-        }
-        if ((textarea.value[i] == "a") && (textarea.value[i + 1] == "i")) {
-            frase = frase + "a";
-            i = i + 1;
-            continue;
-        }
-        if ((textarea.value[i] == "o") && (textarea.value[i + 1] == "b") && (textarea.value[i + 2] == "e") && (textarea.value[i + 3] == "r")) {
-            frase = frase + "o";
-            i = i + 3;
-            continue;
-        }
-        if ((textarea.value[i] == "u") && (textarea.value[i + 1] == "f") && (textarea.value[i + 2] == "a") && (textarea.value[i + 3] == "t")) {
-            frase = frase + "u";
-            i = i + 3;
-            continue;
-        }
-        else {
-            frase = frase + textarea.value[i];
+    for (let i = 0; i < matrizDeCambio.length; i++) {
+        if(textarea.value.includes(matrizDeCambio[i][1])) {
+            textarea.value = textarea.value.replaceAll(matrizDeCambio[i][1], matrizDeCambio[i][0])
         }
     }
+  
+    frase = textarea.value;
     document.querySelector(".msjFinal").innerHTML = frase;
+    textarea.value = "";
     aparece();
 }
 
 function validar() {
 
-    var letras = " abcdefghijklmnñopqrstuvwxyz";
-    var validado;
-    for (var i = 0; i < textarea.value.length; i++) {
+    let letras = " abcdefghijklmnñopqrstuvwxyz";
+    let validado;
+    for (let i = 0; i < textarea.value.length; i++) {
         if (letras.indexOf(textarea.value[i]) == -1) {
             validado = false;
             break;
@@ -100,7 +59,7 @@ function validar() {
 
 function copiar() {
 
-    var textoCopiado = document.querySelector(".msjFinal");
+    let textoCopiado = document.querySelector(".msjFinal");
     textoCopiado.select();
     document.execCommand("copy");
     //para abajo es para limpiar áreas y poner mensajes
@@ -116,11 +75,11 @@ function aparece() {
 }
 
 // Botones
-var boton = document.querySelector(".botonEnc");
+let boton = document.querySelector(".botonEnc");
 boton.onclick = encriptar;
 
-var botonDes = document.querySelector(".botonDes");
+let botonDes = document.querySelector(".botonDes");
 botonDes.onclick = desencriptar;
 
-var botonCopiar = document.querySelector(".botonCop");
+let botonCopiar = document.querySelector(".botonCop");
 botonCopiar.onclick = copiar;
